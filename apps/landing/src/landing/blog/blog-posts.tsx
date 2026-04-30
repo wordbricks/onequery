76d6e2b55e9be3fd4e6fb6851480ee99b393a8ae
Jaeyoun Nam <siisee111@gmail.com>
2026-04-29 19:40:01 -0700

    feat: gepa telemetry use case (#159)
    - **Add GEPA telemetry use case blog post**
    - **feat: update GEPA telemetry blog post**

--- apps/landing/src/landing/blog/blog-posts.tsx
+++ apps/landing/src/landing/blog/blog-posts.tsx
@@ -3,11 +3,13 @@ import type { BlogCategory, BlogPost, BlogPostSection } from "./blog-types";
 import { doNotGiveAgentsProductionKeysPost } from "./posts/do-not-give-agents-production-keys";
 import { howStartupsCanBuildAnInHouseDataAgentPost } from "./posts/how-startups-can-build-an-in-house-data-agent";
 import { makingDataSourceSetupBoringPost } from "./posts/making-data-source-setup-boring";
+import { usingLlmTelemetryToImprovePromptsWithGepaPost } from "./posts/using-llm-telemetry-to-improve-prompts-with-gepa";
 
 export type { BlogCategory, BlogPost, BlogPostSection };
 export { blogCategories, thumbnailCells };
 
 export const blogPosts: BlogPost[] = [
+  usingLlmTelemetryToImprovePromptsWithGepaPost,
   doNotGiveAgentsProductionKeysPost,
   howStartupsCanBuildAnInHouseDataAgentPost,
   makingDataSourceSetupBoringPost,
